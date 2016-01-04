57990aedeed033bc6b20d0990d9fe0e4d65b0ff0
Benom84 <menahem.ben@gmail.com>
2016-01-04 14:28:23 +0200

    Modified UserRankListAdapter + Added answers

--- A16 Ex02 OrSivan 304863418 BenMenahem 039691043/A16 Ex02 OrSivan 304863418 BenMenahem 039691043/UserRankListAdapter.cs
+++ A16 Ex02 OrSivan 304863418 BenMenahem 039691043/A16 Ex02 OrSivan 304863418 BenMenahem 039691043/UserRankListAdapter.cs
@@ -7,23 +7,12 @@ namespace A16_Ex01_OrSivan_304863418_BenMenahem_039691043
 {
     public class UserRankListAdapter<T>
     {
-        private List<UserRank<T>> m_userRankList;
-        public List<UserRank<T>> UserRankList
-        {
-            get
-            {
-                return m_userRankList;
-            }
 
-            set
-            {
-                m_userRankList = value;
-            }
-        }
+        public List<UserRank<T>> UserRankList {get; set;}
 
         private UserRankListAdapter() 
         {
-            m_userRankList = null;
+            UserRankList = null;
         }
         //
 
