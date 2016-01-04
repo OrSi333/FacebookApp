6a3da01edcba63e881d2ef9027d7475b9d74bf58
OrSi333 <orsivan22@gmail.com>
2016-01-04 13:05:49 +0200

    Refactor LazyList

--- A16 Ex02 OrSivan 304863418 BenMenahem 039691043/A16 Ex02 OrSivan 304863418 BenMenahem 039691043/UserRankListAdapter.cs
+++ A16 Ex02 OrSivan 304863418 BenMenahem 039691043/A16 Ex02 OrSivan 304863418 BenMenahem 039691043/UserRankListAdapter.cs
@@ -5,17 +5,18 @@ using System.Text;
 
 namespace A16_Ex01_OrSivan_304863418_BenMenahem_039691043
 {
-    public class LazyList<T>
+    public class UserRankListAdapter<T>
     {
         List<UserRank<T>> m_userRankList;
-        private LazyList(List<UserRank<T>> i_userRankList) 
+        private UserRankListAdapter(List<UserRank<T>> i_userRankList) 
         {
             m_userRankList = i_userRankList;
         }
+        //
 
-        private static LazyList<T> initLazyList(List<UserRank<T>> i_userRankList)
+        private static UserRankListAdapter<T> initUserRankListAdapter(List<UserRank<T>> i_userRankList)
         {
-            return new LazyList<T>(i_userRankList);
+            return new UserRankListAdapter<T>(i_userRankList);
         }
 
     }
